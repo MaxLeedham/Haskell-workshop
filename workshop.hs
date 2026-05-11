dd74cf26aee0a852022be3c525a882ab7df04a63
psyml14 <psyml14@exmail.nottingham.ac.uk>
2026-05-11 16:52:29 +0100

    Section on type declerations

--- workshop.hs
+++ workshop.hs
@@ -1,5 +1,5 @@
 -- Ignore this line, it just lets me redefine existing functions
-import Prelude hiding (sum, map)
+import Prelude hiding (sum, map, String)
 
 sum :: [Int] -> Int
 sum [] = 0
@@ -51,3 +51,20 @@ removeLast (x:xs) = x : removeLast xs
 removeLast :: [a] -> [a]
 removeLast [_] = []
 removeLast (x:xs) = x : removeLast xs
+
+-----------------------------------------
+
+-- type Name = Type
+-- will "rename" / "alias" Type to be Name, letting you use them interchangably
+
+type String = [Char]
+
+-- They are used to convey more meaning in types
+type R2 = (Float, Float)
+
+-- E.g. A function returning (Float, Float) may be returning 2 general floats
+-- but a function returning R2 is returning a point on the 2D plane. 
+
+-- Type definitions can also include arguments
+type Matrix a = [[a]]
+-- E.g. `Matrix Int` will become `[[Int]]`, `Matrix String` will become `[[String]]`
